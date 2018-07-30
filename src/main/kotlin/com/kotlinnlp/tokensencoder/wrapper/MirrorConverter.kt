4692c4c1011803d38db941c6ca95bc4c5797b851
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-30 18:33:14 +0200

    Move MirrorConverter from NeuralParser to TokensEncoder

---
+++ src/main/kotlin/com/kotlinnlp/tokensencoder/wrapper/MirrorConverter.kt
@@ -0,0 +1,27 @@
+/* Copyright 2017-present LHRParser Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * -----------------------------------------------------------------------------*/
+
+package com.kotlinnlp.tokensencoder.wrapper
+
+import com.kotlinnlp.linguisticdescription.sentence.Sentence
+import com.kotlinnlp.linguisticdescription.sentence.token.Token
+
+/**
+ * The sentence converter that mirrors the input sentence to the output.
+ */
+class MirrorConverter<TokenType: Token, SentenceType: Sentence<TokenType>>
+  : SentenceConverter<TokenType, SentenceType, TokenType, SentenceType> {
+
+  /**
+   * Return the same sentence given in input.
+   *
+   * @param sentence the input sentence
+   *
+   * @return the same sentence given in input
+   */
+  override fun convert(sentence: SentenceType): SentenceType = sentence
+}
